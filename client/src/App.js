49f375716caf3273c9b2e5b8ffbf8856aebbd0a5
idrisimo <idrissilva@hotmail.com>
2022-04-19 10:52:29 +0100

    removing some old test code.

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,17 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, } from './actions';
-
 import {Header} from './layout'
 import {Jumbotron} from './components'
 import {QuizPage} from './pages'
-import { Button } from 'react-bootstrap';
+
 
 
 function App() {
-  const counter = useSelector(state => state.counter)
-  const dispatch = useDispatch()
+
   return (
     <div className="App">
       <Header/>
-      <h1>Counter {counter} </h1>
-      <Button onClick={() => dispatch(increment())}>+</Button>
-      <Button onClick={() => dispatch(decrement())}>-</Button>
       <main>
         <Routes>
           <Route path="/" element={<Jumbotron/>}/>
