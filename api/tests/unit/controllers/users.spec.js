87b72bab3d9c95582eeb9a75ffba6c7cd6c184e5
Marco <marcoporcaro87@gmail.com>
2022-04-19 16:57:15 +0100

    adding users and user testing

--- api/tests/unit/controllers/users.spec.js
+++ api/tests/unit/controllers/users.spec.js
@@ -14,10 +14,26 @@ describe('users controller', () => {
     describe('index', () => {
         test('it returns users with a 200 status code', async () => {
             jest.spyOn(User, 'all', 'get')
-                 .mockResolvedValue(['Goku', 'Majin Buu']);
-            await usersController.index(null, mockRes);
+                 .mockResolvedValue(['user1', 'user2']);
+            await usersController.getAll(null, mockRes);
             expect(mockStatus).toHaveBeenCalledWith(200);
-            expect(mockJson).toHaveBeenCalledWith(['Goku', 'Majin Buu']);
+            expect(mockJson).toHaveBeenCalledWith(['user1', 'user2']);
         })
     });
+
+    describe('show', () => {
+        test('it returns an user with a 200 status code', async () => {
+            jest.spyOn(User, 'findByUser')
+                .mockResolvedValue(new User({ id: 1, username: 'Marco'} ));
+                
+            const mockReq = { params: { username: "Marco" } }
+            await usersController.getUser(mockReq, mockRes);
+            expect(mockStatus).toHaveBeenCalledWith(200);
+            expect(mockJson).toHaveBeenCalledWith({
+                id : 1,
+                username: 'Marco'
+            });
+        })
+    });
+    
 })
--- api/tests/unit/models/user.spec.js
+++ api/tests/unit/models/user.spec.js
@@ -10,12 +10,32 @@ describe('User', () => {
     afterAll(() => jest.resetAllMocks())
 
     describe('all', () => {
-        test('it resolves with users on successful db query', async () => {
+        test('it resolves with authors on successful db query', async () => {
             jest.spyOn(db, 'query')
                 .mockResolvedValueOnce({ rows: [{}, {}, {}]});
             const all = await User.all;
             expect(all).toHaveLength(3)
         })
     });
-    
+
+    describe('findByUser', () => {
+        test('it resolves with user on successful db query', async () => {
+            let userData = { id: 1, username: 'Marco' }
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({rows: [ userData] });
+            const result = await User.findByUser("Marco");
+            expect(result).toBeInstanceOf(User)
+        })
+    });
+
+    describe('create', () => {
+        test('it resolves with user on successful db query', async () => {
+            let userData = { id: 1, username: 'Marco' }
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({rows: [ userData] });
+            const result = await User.createUser('Marco');
+            expect(result).toBeInstanceOf(User)
+        })
+    });
+
 })
