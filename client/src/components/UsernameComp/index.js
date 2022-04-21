7ec00b4ae31ad2bfc0acb9762dc8ff5d48431512
idrisimo <idrissilva@hotmail.com>
2022-04-21 17:26:21 +0100

    added a check for username in database

--- client/src/components/UsernameComp/index.js
+++ client/src/components/UsernameComp/index.js
@@ -3,15 +3,24 @@ import { Box } from "@mui/system";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { usernameInput } from "../../actions";
-
+import axios from 'axios'
 export const UsernameComp = ({playerId}) => {
   const dispatch = useDispatch();
+
+  const checkUserExists = async(username) => {
+    try{
+      await axios.get(`https://fpquizwar.herokuapp.com/users/${username}`)
+    } catch (err) {
+      await axios.post('https://fpquizwar.herokuapp.com/users', {username:username})
+    }
+  }
+
   const handleChange = (e) => {
     console.log('handle change')
     if(e.key ==='Enter') {
+      checkUserExists(e.target.value)
       dispatch(usernameInput(playerId, e.target.value));
     }
-    
   };
 
   return (
